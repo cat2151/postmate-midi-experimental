53d379cbadf10f029debda84b0a90c7687adbf9b
cat2151 <fct_2004@mail.goo.ne.jp>
2024-04-26 23:04:33 +0900

    experimental21 save wav dialog 土台

--- experimental21/postmate-midi.js
+++ experimental21/postmate-midi.js
@@ -891,6 +891,10 @@ function sendWavAfterHandshakeAllChildrenSub(wavs) {
   postmateMidi.parent.emit('sendToSampler' + (postmateMidi.childId + 1), wavs);
   // samplerのprerenderボタンを押したあと、seqのplayボタンで演奏できるようにする用
   if (postmateMidi.hasPreRenderButton) postmateMidi.isPreRenderSynth = false;
+  saveWavDialog(wavs);
+}
+
+function saveWavDialog(wavs) {
   // TODO dialogを表示して、ファイル保存できるようにする
 }
 
