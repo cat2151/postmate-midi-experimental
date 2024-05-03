031b668d3955b541db6b5525aa046ef7310a5b82
cat2151 <fct_2004@mail.goo.ne.jp>
2024-05-03 13:18:19 +0900

    experimental21 synthとsamplerは、synth-poly.jsに統合した

--- experimental21/sampler/sampler-child.js
+++ experimental21/sampler/sampler-child.js
@@ -1,13 +1,13 @@
 import { postmateMidi } from "../postmate-midi.js";
-import { initSampler } from "./sampler.js";
+import { initSynth } from "../synth/synth-poly.js";
 if (!new URL(window.location.href).searchParams.get('query')) window.location.href = "../"; // dir階層直叩きした場合の混乱防止用
 const urlParams = rison2.parse(new URL(window.location.href).searchParams.get('query'));
 
 postmateMidi.registerChild(urlParams, null, null, null, null, null);
 
 postmateMidi.tonejs.registerSynth((ch) => {
-  initSampler(ch[1-1], null, /*volume=*/-9);
-  initSampler(ch[2-1], null, /*volume=*/-6);
+  initSynth(ch[1-1], Tone.Sampler, null, /*volume=*/-9);
+  initSynth(ch[2-1], Tone.Sampler, null, /*volume=*/-6);
 });
 
 postmateMidi.ui.registerPrerenderButton('#prerender');
