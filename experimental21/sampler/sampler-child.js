91398c9d785527ab9b0867462ad74c1e8fff11ca
cat2151 <fct_2004@mail.goo.ne.jp>
2024-05-30 22:11:54 +0900

    experimental21 import prerender.js sampler

--- experimental21/sampler/sampler-child.js
+++ experimental21/sampler/sampler-child.js
@@ -1,4 +1,5 @@
 import { postmateMidi } from "../postmate-midi.js";
+import { prerender } from "../prerender.js";
 import { initSynth } from "../synth/synth-poly.js";
 if (!new URL(window.location.href).searchParams.get('query')) window.location.href = "../"; // dir階層直叩きした場合の混乱防止用
 const urlParams = rison2.parse(new URL(window.location.href).searchParams.get('query'));
