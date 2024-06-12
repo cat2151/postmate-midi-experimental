d23ab9816b5236cb41bccf017f41089fc9aa7d3a
cat2151 <fct_2004@mail.goo.ne.jp>
2024-06-12 22:07:49 +0900

    experimental21 prerender renderContextAsync comment

--- experimental21/postmate-midi.js
+++ experimental21/postmate-midi.js
@@ -931,6 +931,7 @@ function schedulingPreRender(gn, preRenderMidi) {
 }
 
 // TODO 部分的に、prerender側に切り出す。ここの業務ロジックは、用途に応じていくらでも変化しうる想定。
+// ここはまるごとprerender側に切り出す想定。その際、checkWavOkは、postmateMidiの公開API、にひとまずする想定。
 async function renderContextAsync(gn, context, orgContext, songId) {
   const startTime = Date.now();
   console.log(`${getParentOrChild()} : Tone.js wav preRendering : start... : songId ${songId} : time : ${Date.now() % 10000}`);
