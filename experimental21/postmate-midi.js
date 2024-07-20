080eb92aac3144a2f746fe40186d6b2c5576fd11
cat2151 <fct_2004@mail.goo.ne.jp>
2024-07-20 13:46:41 +0900

    experimental21 prerender sendWavAfterHandshakeAllChildrenSub comment

--- experimental21/postmate-midi.js
+++ experimental21/postmate-midi.js
@@ -964,7 +964,7 @@ function setContextInitSynthAddWav(context) {
 
 // TODO 部分的に、prerender側に切り出す。ここの業務ロジックは、用途に応じていくらでも変化しうる想定。
 // まるごと切り出す想定。呼び出しているfncは公開APIにひとまずする想定。
-// TODO テストケースを書く
+// TODO テストケースを書く。例、prerender側に移動して、呼び出し元から呼び出して、prerender後に、sendToSamplerの結果として音が鳴ること。wav保存ダイアログが出ること。logが出力されること。
 function sendWavAfterHandshakeAllChildrenSub(wavs) {
   if (!postmateMidi.isChild) return; // 備忘、parentは送受信の対象外にしておく、シンプル優先
   console.log(`${getParentOrChild()} : sendWavAfterHandshakeAllChildrenSub : time : ${Date.now() % 10000}`);
