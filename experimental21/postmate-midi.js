36c5504118ddc16169e64c4a7b637160b5eeeb96
cat2151 <fct_2004@mail.goo.ne.jp>
2024-08-14 16:33:42 +0900

    experimental21 prerender onStartPreRender comment

--- experimental21/postmate-midi.js
+++ experimental21/postmate-midi.js
@@ -883,7 +883,7 @@ function isPreRenderSynth() {
 // TODO 関数全体を、prerender側に切り出す。ここの業務ロジックは、用途に応じていくらでも変化しうる想定。
 // prerenderにはpostmateMidiをわたせることを動作確認済み。なので、sqにはアクセスできる想定。
 // emit呼び出しも、postmateMidiのコア部分ではあるが、まず外部prerendererでやれるか試す想定。つまり関数全体を切り出しを試す想定。
-// TODO テストケースを書く。例、prerenderされたwavがplayボタンで鳴ること。これが実行された結果、これのlogが出ること。
+// テストケース : prerenderされたwavがplayボタンで鳴ること。これが実行された結果、これのlogが出ること。
 function onStartPreRender(data) {
   // sq
   if (postmateMidi.ui.checkRemovePlayButton) postmateMidi.ui.checkRemovePlayButton(); // playボタンを消す用。混乱防止用。playボタンがあると混乱する。
