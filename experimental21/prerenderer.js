d0b8e487baaa955b3da574a4a0fa77e5750b6dfe
cat2151 <fct_2004@mail.goo.ne.jp>
2024-11-11 23:37:06 +0900

    experimental21 prerender visualizeGeneratedSound comment

--- experimental21/prerenderer.js
+++ experimental21/prerenderer.js
@@ -283,7 +283,7 @@ function getChNum(filename) {
 //      wav import後にも呼び出すし、prerender後にも呼び出す
 //      具体的にはどこから？
 //       TODO visualizeGeneratedSound_dispWavsSub の呼び出し元の候補が2つある想定なのでlistupする
-//        wav import 後 :
+//        wav import 後 : afterWavFileUploadAsync の末尾を想定。そこは「sampler側のwav import」なので、実装してから検証が必要そう。
 //        prerender 後 :
 //         generator側 :
 //         sampler側 :
