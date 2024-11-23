f03fb95eab4bffe0710e384ab3a47d4043a24955
cat2151 <fct_2004@mail.goo.ne.jp>
2024-11-23 20:37:53 +0900

    experimental21 prerender visualizeGeneratedSound comment

--- experimental21/prerenderer.js
+++ experimental21/prerenderer.js
@@ -284,7 +284,7 @@ function getChNum(filename) {
 //   済 sampler側 : 起動時にgeneratorでrenderしてsamplerにsendされた波形が、sampler側に波形表示されること。
 //   sampler側 : wav importしたとき、importした波形が、sampler側に波形表示されること。 ※現在は更新されていない認識
 //   sampler側 : `prerender`ボタンでself samplingしたとき、self sampling後の波形が、sampler側に波形表示されること。 ※現在はprerenderボタンが動作しないのでそれを修正するのが先
-//   generator側 : （呼び出し構造変更後、リグレッションテスト）起動時にgeneratorでrendarしたあと、その波形がgeneratorに表示されること。
+//   generator側 : （呼び出し構造変更後、リグレッションテスト）起動時にgeneratorでrenderしたあと、その波形がgeneratorに表示されること。
 //   TODO list upする
 // TODO wav import後にも呼び出して、描画する。
 //  test case : 異なるwavをimportするごとに、それに応じた波形が表示されること
