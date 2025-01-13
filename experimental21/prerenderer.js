07f53fc4ffbe169eec91faf0acc817dfca1a67c6
cat2151 <fct_2004@mail.goo.ne.jp>
2025-01-13 15:45:32 +0900

    experimental21 prerender visualizeGeneratedSound comment

--- experimental21/prerenderer.js
+++ experimental21/prerenderer.js
@@ -313,7 +313,9 @@ function getChNum(postmateMidi, filename) {
 //          済 起動時、自動prerenderのち、generator側に波形が表示され、sampler側にも同じ波形が表示されること。
 //          済 prerenderボタンを押したのち、sampler側に新たな波形が表示されること。generator側の波形は変化がないこと。
 //    TODO 上記がtest greenになった。event登録が不要になる想定で、event登録を外してtestする。
-//           ■ event登録が不要になることを確認する、つまりregisterEventが不要になることを確認する
+//           済 event登録が不要になることを確認する、つまり registerEvent が不要になることを確認する
+//              → 確認した。registerEvent なしでよい、と判断する。根拠は、 samplerAddWavs 末尾で呼ばれていること。あとは実際に動作させる。
+//                もし想定外に、起動時に表示されない等あれば、samplerAddWavs 末尾同様に、要所にfnc呼び出し追加、を想定している。
 
 // TODO child2にてprerender完了時に呼び出して、描画する。描画のトリガーは「prerender完了時」にする。
 
